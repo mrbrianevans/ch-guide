70cce9e23ff4d806e175571141ca41d93e65b9b1
Brian Evans <ebrian101@gmail.com>
2022-08-16 11:51:50 +0100

    Fix config, update packages, gen sitemap
    Just a few chores. No content changes. Edit link wasn't showing due to a mistake in the config.
    
    Signed-off-by: Brian Evans <ebrian101@gmail.com>

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,54 +1,56 @@
 /**
- * @type {import('vitepress').UserConfig}
+ * @type {import("vitepress").UserConfig}
  */
 const config = {
-  title: "CH Guide",
-  description: 'Unofficial Companies House developer guide',
-  outDir: '../build',
-  lang: 'en-GB',
-  lastUpdated: true,
-  editLink: {
-    pattern: 'https://github.com/mrbrianevans/ch-guide/edit/master/docs/:path'
-  },
-  themeConfig: {
-    nav: [
-      {
-        text: 'Bulk data',
-        items: [
-          { text: 'Overview', link: '/bulk-data' },
-          { text: 'Companies', link: '/bulk-data/companies' },
-          { text: 'Persons with significant control', link: '/bulk-data/psc' },
-          { text: 'Officers', link: '/bulk-data/officers' },
-          { text: 'Accounts', link: '/bulk-data/accounts/bulk-file' }
-        ]
-      },
-      {text: 'Streaming API', link: '/streams'},
-      {text: 'REST API', link: '/rest-api'}
-    ],
-    sidebar: [
-      {
-        text: 'Bulk data',
-        items: [
-          { text: 'Overview', link: '/bulk-data' },
-          { text: 'Companies', link: '/bulk-data/companies' },
-          { text: 'Persons with significant control', link: '/bulk-data/psc' },
-          { text: 'Officers', link: '/bulk-data/officers' },
-          { text: 'Accounts', link: '/bulk-data/accounts/bulk-file' }
-        ]
-      },
-      {text: 'APIs', items: [
-          {text: 'Streaming API', link: '/streams'},
-          {text: 'REST API', link: '/rest-api'}
-
-        ]}
-    ],
-    footer: {
-      message: 'Made with ♥ by Brian Evans'
-    },
-    socialLinks: [
-      { icon: 'github', link: 'https://github.com/mrbrianevans/ch-guide' },
-    ]
+  "title": "CH Guide",
+  "description": "Unofficial Companies House developer guide",
+  "outDir": "../build",
+  "lang": "en-GB",
+  "lastUpdated": true,
+  "themeConfig": {
+    "editLink": {
+      "pattern": "https://github.com/mrbrianevans/ch-guide/edit/master/docs/:path",
+      "text": "Edit this page on GitHub"
+    }, "nav": [{
+      "text": "Bulk data", "items": [{
+        "text": "Overview", "link": "/bulk-data"
+      }, {
+        "text": "Companies", "link": "/bulk-data/companies"
+      }, {
+        "text": "Persons with significant control", "link": "/bulk-data/psc"
+      }, {
+        "text": "Officers", "link": "/bulk-data/officers"
+      }, {
+        "text": "Accounts", "link": "/bulk-data/accounts/bulk-file"
+      }]
+    }, {
+      "text": "Streaming API", "link": "/streams"
+    }, {
+      "text": "REST API", "link": "/rest-api"
+    }], "sidebar": [{
+      "text": "Bulk data", "items": [{
+        "text": "Overview", "link": "/bulk-data"
+      }, {
+        "text": "Companies", "link": "/bulk-data/companies"
+      }, {
+        "text": "Persons with significant control", "link": "/bulk-data/psc"
+      }, {
+        "text": "Officers", "link": "/bulk-data/officers"
+      }, {
+        "text": "Accounts", "link": "/bulk-data/accounts/bulk-file"
+      }]
+    }, {
+      "text": "APIs", "items": [{
+        "text": "Streaming API", "link": "/streams"
+      }, {
+        "text": "REST API", "link": "/rest-api"
+      }]
+    }], "footer": {
+      "message": "Made with ♥ by Brian Evans"
+    }, "socialLinks": [{
+      "icon": "github", "link": "https://github.com/mrbrianevans/ch-guide"
+    }]
   }
-}
+};
 
-export default config
+export default config;
